94d758cd5a317079b1d5775399810a68e1271f94
jvsmatheus <matheusjvs_@hotmail.com>
2026-01-23 20:21:24 -0300

    chore: rotas de login e register sem service

--- src/main/java/com/organizas/organizas/controllers/AuthController.java
+++ src/main/java/com/organizas/organizas/controllers/AuthController.java
@@ -1,6 +1,61 @@
 package com.organizas.organizas.controllers;
 
+import com.organizas.organizas.dto.request.LoginRequestDto;
+import com.organizas.organizas.dto.request.RegisterUserRequestDto;
+import com.organizas.organizas.dto.response.LoginResponseDto;
+import com.organizas.organizas.dto.response.RegisterUserResponseDto;
+import com.organizas.organizas.entities.User;
+import com.organizas.organizas.repositories.UserRepository;
+import com.organizas.organizas.security.UserDetailsImpl;
+import com.organizas.organizas.utils.JwtUtils;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto request) {
+        var userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        var authentication = authenticationManager.authenticate(userAndPass);
+
+        var user = (UserDetailsImpl) authentication.getPrincipal();
+        var token = jwtUtils.generateToken(user);
+        return ResponseEntity.status(HttpStatus.OK).body(new LoginResponseDto(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterUserResponseDto> register(@Valid @RequestBody RegisterUserRequestDto request) {
+        var newUser = new User();
+        newUser.setPassword(passwordEncoder.encode(request.password()));
+        newUser.setEmail(request.email());
+        newUser.setName(request.name());
+
+        userRepository.save(newUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new RegisterUserResponseDto(
+                newUser.getName(), newUser.getEmail()
+        ));
+    }
 }
